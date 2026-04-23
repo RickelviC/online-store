cb512e3d5dd893ef49f39212128d866eb7f03da7
RickelviC <Cruzrickelvi@gmail.com>
2026-04-23 14:25:32 -0400

    added readme and Product file

--- src/main/java/com/pluralsight/Product.java
+++ src/main/java/com/pluralsight/Product.java
@@ -1,4 +1,33 @@
 package com.pluralsight;
 
 public class Product {
+    private String id;
+    private String name;
+    private double price;
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public Product(String id, String name, double price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
 }
